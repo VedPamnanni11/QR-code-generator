35db78af76a9f4d7449d46b84466fbaff20b6b34
Ved Pamnani <152010733+VedPamnanni11@users.noreply.github.com>
2025-02-01 20:15:00 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import qrcode as qr
 
-url = qr.make(input("Enter a URL or text for a qr code : "))
-url.save(input("What do you want the file to be saved as?, and don't forget to add '.png' at the end : "))
+while True:
+
+    url = qr.make(input("Enter a URL or text for a qr code : "))
+    url.save(input("What do you want the file to be saved as?, and don't forget to add '.png' at the end : "))
